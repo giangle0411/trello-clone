d5833ddb510673ad361239b1ae4248a68fa825d9
Giang-Le <giangle0411@gmail.com>
2020-06-05 18:52:41 +1000

    Created mixins

--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -22,25 +22,15 @@
 </template>
 
 <script>
+import movingTasksColumnsMixin from '@/mixins/movingTasksColumnsMixin'
 export default {
+  mixins: [movingTasksColumnsMixin],
   props: {
     task: {
       type: Object,
       required: true
     },
-    column: {
-      type: Object,
-      required: true
-    },
-    board: {
-      type: Object,
-      required: true
-    },
     taskIndex: {
-      type: Number,
-      required: true
-    },
-    columnIndex: {
       type: Number,
       required: true
     }
@@ -56,37 +46,6 @@ export default {
     },
     goToTask(task) {
       this.$router.push({ name: 'task', params: { id: task.id } })
-    },
-    moveTaskOrColumn(e, toTasks, toColumnIndex, toTaskIndex) {
-      const type = e.dataTransfer.getData('type')
-      if (type === 'task') {
-        this.moveTask(
-          e,
-          toTasks,
-          toTaskIndex !== undefined ? toTaskIndex : toTasks.length
-        )
-      } else {
-        this.moveColumn(e, toColumnIndex)
-      }
-    },
-    moveTask(e, toTasks, toTaskIndex) {
-      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-      const fromTasks = this.board.columns[fromColumnIndex].tasks
-      const fromTaskIndex = e.dataTransfer.getData('from-task-index')
-
-      this.$store.commit('MOVE_TASK', {
-        fromTasks,
-        toTasks,
-        fromTaskIndex,
-        toTaskIndex
-      })
-    },
-    moveColumn(e, toColumnIndex) {
-      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-      this.$store.commit('MOVE_COLUMN', {
-        fromColumnIndex,
-        toColumnIndex
-      })
     }
   }
 }
